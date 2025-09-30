d99fcf159070d1721adfed40ac604f7f46877f3b
Junior7 <giftmomanyi97@gmail.com>
2025-09-30 22:36:40 +0300

    feat:models for user, booking and vehicles cerated

--- jobunyacar/settings.py
+++ jobunyacar/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest-framework'
+    'rest_framework',
+    'rental_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
