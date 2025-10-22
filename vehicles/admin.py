07f13abdc9d8a275793183b2d63f0bd3f8c4d352
Junior7 <giftmomanyi97@gmail.com>
2025-10-22 15:31:56 +0300

    feat:added vehicle image to admin

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Vehicle
+from .models import Vehicle, VehicleImage
 
 # Register your models here.
+class VehicleImageInline(admin.TabularInline):
+    """Allows adding multiple VehicleImage objects directly in the Vehicle form."""
+    model = VehicleImage
+    extra = 1  # how many empty forms to show
+    fields = ('image', 'uploaded_at')
+    readonly_fields = ('uploaded_at',)
+
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ['name', 'model', 'car_type', 'seats', 'daily_rate', 'status', 'created_at']
     search_fields = ['name', 'model', 'car_type', 'created_at']
+    inlines = [VehicleImageInline]
 
-admin.site.register(Vehicle, VehicleAdmin)
+
+@admin.register(VehicleImage)
+class VehicleImageAdmin(admin.ModelAdmin):
+    list_display = ['vehicle', 'image', 'uploaded_at']
+    search_fields = ['vehicle__name']
