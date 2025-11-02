a9394488577e4dd5a63bcc83b0a77846bfdf418a
Junior7 <giftmomanyi97@gmail.com>
2025-11-02 14:35:10 +0300

    fix:increased pagination

--- jobunyacar/settings.py
+++ jobunyacar/settings.py
@@ -172,7 +172,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 1000,
 }
 
 # For password reset email in dev:
