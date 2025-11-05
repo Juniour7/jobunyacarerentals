513cb7ab7862f6d9d0f2c241cebb6cbc6f69e6f4
Junior7 <giftmomanyi97@gmail.com>
2025-11-05 08:33:20 +0300

    fix:setting for email sending

--- jobunyacar/settings.py
+++ jobunyacar/settings.py
@@ -206,6 +206,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  # your Gmail address
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  # app password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
